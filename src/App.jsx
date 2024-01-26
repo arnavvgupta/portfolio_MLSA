e0756a6a108e27925f95b6eab253770c0847e09f
Arnav Gupta <arnavgupta@Arnavs-MacBook-Air.local>
2024-01-26 12:41:28 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Arnav Gupta",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "arnavgupta661@gmail.com",
+  gitHub: "arnavvgupta",
+  instagram: "arnavvv_gupta",
+  linkedIn: "arnav-gupta-b1715121b",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "arnavgupta661",
+  youTube: "@ArnavvGupta",
 };
 
 const primaryColor = "#4E567E";
